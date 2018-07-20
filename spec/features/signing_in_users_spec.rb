cb1bee5b800f08f84daf73825ce4cd5094c7d52e
Matt Berry <mberry41@gmail.com>
2018-07-19 19:35:14 -0600

    Added signing_up_users.spec

---
+++ spec/features/signing_in_users_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "Signup users", js: true do
+
+# click_link "Login"
+        # fill_in "Email", with: "user@example.com"
+        # fill_in "Password", with: "password"
+        # click_button "Submit"
+
+end
--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -9,25 +9,22 @@ feature "Signup users", js: true do
         fill_in "Password", with: "password"
         fill_in "Password Confirmation", with: "password"
         click_button "Submit"
-
+        
+        expect(page).to have_current_path("/")
         page.find(:xpath, "//a[text()='Logout']").click
-
-        click_link "Login"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "password"
-        click_button "Submit"
-
-        expect(page).to have_content("Welcome to BetaVault!")
+        expect(page).to have_content("Logged out successfully!")
+    
     end
 
-    # scenario "with invalid credentials" do
-    #     visit "http://localhost:3000"
-    #     click_on "Register"
-    #     fill_in "Email", with: ""
-    #     fill_in "Password", with: ""
-    #     fill_in "Password Confirmation", with: ""
-    #     click_button "Submit"
+    scenario "with invalid credentials" do
+        visit "http://localhost:3000"
+        click_on "Register"
+        fill_in "Email", with: ""
+        fill_in "Password", with: ""
+        fill_in "Password Confirmation", with: ""
+        click_button "Submit"
 
-    #     expect(page).to have_content("You have not signed up successfully.")
-    # end
+        expect(page).to have_current_path("/register")
+        expect(page).to have_content('Login')
+    end
 end
