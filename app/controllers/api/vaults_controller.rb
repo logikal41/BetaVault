905d41bf8ecec29b88c8c8239faa2473b8674c1f
Matt Berry <mberry41@gmail.com>
2018-07-19 23:11:14 -0600

    Added vaults controller

---
+++ app/controllers/api/vaults_controller.rb
@@ -0,0 +1,15 @@
+class Api::VaultsController < ApplicationController
+  before_action :authenticate_user! 
+
+  def index
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
