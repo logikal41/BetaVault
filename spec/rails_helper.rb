740e20bbc313c2f17793810fbe45f306ad8d627c
Matt Berry <mberry41@gmail.com>
2018-07-23 22:30:07 -0600

    cleaned up the rails_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,10 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-# Add additional requires below this line. Rails is not loaded until this point!
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
 require 'selenium-webdriver'
 # require 'shoulda/matchers'
