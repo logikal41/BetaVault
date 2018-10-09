1f9d4bc99482437040982db488266ee14f462551
Matt Berry <mberry41@gmail.com>
2018-10-08 22:03:33 -0600

    updated the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Segment, Form, Button, Image, Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import { handleLogin } from '../actions/auth';
+import BetaVaultBox from '../images/BetaVault_Box.png';
 
 class Home extends Component {
+  state = { email: '', password: '' };
+
+  handleChange = event => {
+    const { id, value } = event.target;
+    this.setState({ [id]: value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { dispatch, history } = this.props;
+    const { email, password } = this.state;
+    dispatch(handleLogin(email, password, history));
+  }
+
   render() {
+    const { email, password } = this.state;
+    const { history } = this.props;
+
     return (
-      <Header as='h1' textAlign='center'>Welcome to BetaVault!</Header>
+      <Grid centered="true">
+        <Grid.Row>
+          <Segment basic>
+            <Image src={BetaVaultBox} centered="true" size="small"/>
+            <Header as='h1' textAlign='center'>Welcome to The Beta Vault!</Header>
+          </Segment>
+        </Grid.Row>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label htmlFor='email'>Email</label>
+            <input
+              required
+              id='email'
+              value={email}
+              placeholder='Email'
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label htmlFor='password'>Password</label>
+            <input
+              required
+              id='password'
+              value={password}
+              placeholder='Password'
+              type='password'
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Grid.Row className="home-buttons">
+            <Button primary type='submit'>Submit</Button>
+          </Grid.Row>
+          <Grid.Row className="home-buttons">
+            <Button secondary onClick={() => history.push("/register")}>Register</Button>
+          </Grid.Row>
+        </Form>
+      </Grid>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = ({history}) => {
+  return { history }
+};
+
+
+export default withRouter(connect()(Home));
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Image } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleLogout } from '../actions/auth';
+import BetaVaultLogo from '../images/BetaVault_Word.png';
 
 class NavBar extends Component {
   rightNavs = () => {
@@ -34,6 +35,7 @@ class NavBar extends Component {
     return (
       <div>
         <Menu pointing secondary>
+        {/* <Image src={BetaVaultLogo} inline="true" size="mini" /> */}
           <Link to='/'>
             <Menu.Item name='home' />
           </Link>
