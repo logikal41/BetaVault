ffac6d76761784bc4bc7841b18a79ad1c8ffee0b
Matt Berry <mberry41@gmail.com>
2018-10-17 21:12:10 -0600

    addition of the list_vault test spec" ;

--- app/models/vault.rb
+++ app/models/vault.rb
@@ -3,5 +3,5 @@ class Vault < ApplicationRecord
     has_many :areas, dependent: :destroy
 
     # This will chagne the scope instead of calling .order in controller
-    # default_scope { order(:id asc:)}
+    # default_scope { order(id: :asc)}
 end
---
+++ spec/features/list_vaults.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature "List vaults", js: true do
+
+    before do
+        @user = User.create(name: "user", email: "user@test.com", password: "password")
+        @vault1 = Vault.create(name: 'vault one', description: 'description of the first vault')
+        @vault2 = Vault.create(name: 'vault two', description: 'description of the second vault')
+
+        # the database should reject this since it shares the same name as vault 2
+        @vault3 = Vault.create(name: 'vault two', description: 'this vault should not exist')
+        
+        visit "http://localhost:3000"
+        fill_in "email", with: @user.email
+        fill_in "password", with: @user.password
+        click_button "Login"
+    end
+
+    scenario 'vaults list correctly' do
+        expect(page).to have_content(@vault1.name)
+        expect(page).to have_content(@vault1.description)
+        expect(page).to have_content(@vault2.name)
+        expect(page).to have_content(@vault2.description)
+        expect(page).not_to have_content(@vault3.description)
+        expect(page).to have_content("Logout")
+    end
+    
+
+
+end
