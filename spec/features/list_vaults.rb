85aa14be29db5aac8bbbe9629a5656974c16ab69
Matt Berry <mberry41@gmail.com>
2018-10-17 22:30:48 -0600

    change to list vaults feature spec

--- spec/features/list_vaults.rb
+++ spec/features/list_vaults.rb
@@ -17,7 +17,7 @@ feature "List vaults", js: true do
         click_button "Login"
     end
 
-    scenario 'vaults list correctly' do
+    scenario 'all vaults are listed' do
         expect(page).to have_content(@vault1.name)
         expect(page).to have_content(@vault1.description)
         expect(page).to have_content(@vault2.name)
@@ -26,6 +26,22 @@ feature "List vaults", js: true do
         expect(page).to have_content("Logout")
     end
     
+    scenario 'there are no vaults' do
+        #Remove all instance vaults
+        Vault.delete_all
 
+        expect(page).not_to have_content(@vault1.name)
+        expect(page).not_to have_content(@vault1.description)
+        expect(page).not_to have_content(@vault2.name)
+        expect(page).not_to have_content(@vault2.description)
+        expect(page).to have_content("Logout")
+
+        # put some code here to test that "NO vaults created" shows up
+        # when the list is empty
+
+        # within ("h1#no-articles") do
+        #     expect(page).to have_content("No Vaults Created")
+        # end
+    end
 
 end
